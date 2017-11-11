c655cfe3b95bf438925878c2e3e43b22a50b8479
manipandi <manirathnam.pandi@a-cti.com>
2017-11-11 16:00:30 +0530

    modified file

--- test.js
+++ test.js
@@ -2,3 +2,7 @@ var myEmitter = new MyEmitter();
 myEmitter.on('event', () => {});
 myEmitter.on('event', () => {});
 console.log(EventEmitter.listenerCount(myEmitter, 'event'));
+
+
+
+new line added
